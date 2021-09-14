0d405b0693f206121d82b74d856bbf84ba914748
IZUMI HIGASHIYAMA <higashi10227193@gmail.com>
2021-09-14 15:24:41 +0900

    Rails 6にwebpackkerでjQueryとBootstrapを入れる

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import 'bootstrap'
-import '../stylesheets/application'
+import 'bootstrap';
+import '../stylesheets/application';
 require.context('../images', true)
 
 
